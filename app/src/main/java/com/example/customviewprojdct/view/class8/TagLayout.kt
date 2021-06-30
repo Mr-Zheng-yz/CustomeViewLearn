3634362b204271028129e2ea4d5d282b983387cb
baize <zhengyz_0430@qq.com>
2021-06-30 19:23:08 +0800

    完善TagLayout

--- app/src/main/java/com/example/customviewprojdct/view/class8/TagLayout.kt
+++ app/src/main/java/com/example/customviewprojdct/view/class8/TagLayout.kt
@@ -21,26 +21,38 @@ class TagLayout @JvmOverloads constructor(
         //  1）开发者配置（父 View 对我的限制，开发者对子 View 的限制），从layoutParams获取    2）可用空间： 父View限制；除去已使用部分；
         //2. 保存子 View 测量结果
         //3. 根据所有子 View 测量结果确定自己的宽高
-        var widthUsed = 0
+        var lineWidthUsed = 0
         var heightUsed = 0
         var lineMaxHeight = 0
+        var lineMaxWidth = 0
+        val parentWidthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val parentWidthSize = MeasureSpec.getSize(widthMeasureSpec)
         for ((index, child) in children.withIndex()) {
             //测量子View，用0去测得到子View真实宽度
             measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, heightUsed)
+            //换行时机：测到的子View宽度大于一行剩下的宽度，则换行
+            if (parentWidthMode != MeasureSpec.UNSPECIFIED && child.measuredWidth > parentWidthSize - lineWidthUsed) {
+                lineWidthUsed = 0
+                heightUsed += lineMaxHeight
+                lineMaxHeight = 0
+                //重新测量（只重新测量一次，由于布局特性，测量多次结果一样）
+                measureChildWithMargins(child, widthMeasureSpec, lineWidthUsed, heightMeasureSpec, heightUsed)
+            }
 
             //存储测量结果
             if (index >= childrenBounds.size) {
-                childrenBounds.add(Rect(widthUsed, heightUsed, widthUsed + child.measuredWidth, child.measuredHeight))
-            } else {
-                childrenBounds[index].set(widthUsed, heightUsed, widthUsed + child.measuredWidth, child.measuredHeight)
+                childrenBounds.add(Rect())
             }
-            widthUsed += child.measuredWidth
+            childrenBounds[index].set(lineWidthUsed, heightUsed, lineWidthUsed + child.measuredWidth, heightUsed + child.measuredHeight)
+
+            lineWidthUsed += child.measuredWidth
+            lineMaxWidth = max(lineMaxWidth,lineWidthUsed)
             lineMaxHeight = max(lineMaxHeight, child.measuredHeight)
         }
 
         //确定自己宽高
-        val selfWidth = resolveSizeAndState(widthUsed, widthMeasureSpec, 0)
-        val selfHeight = resolveSizeAndState(heightUsed, heightMeasureSpec, 0)
+        val selfWidth = resolveSizeAndState(lineMaxWidth, widthMeasureSpec, 0)
+        val selfHeight = resolveSizeAndState(heightUsed + lineMaxHeight, heightMeasureSpec, 0)
         setMeasuredDimension(selfWidth, selfHeight)
     }
 
