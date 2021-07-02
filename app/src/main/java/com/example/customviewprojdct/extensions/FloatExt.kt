4d3e1fb4aec215b3bf618e6a6fa0bf68e539b5a0
baize <zhengyz_0430@qq.com>
2021-07-02 12:36:16 +0800

    加入ScaleableImageView

--- app/src/main/java/com/example/customviewprojdct/extensions/FloatExt.kt
+++ app/src/main/java/com/example/customviewprojdct/extensions/FloatExt.kt
@@ -1,15 +1,35 @@
 package com.example.customviewprojdct.extensions
 
 import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.TypedValue
+import com.example.customviewprojdct.R
 
 val Float.dp
     get() : Float {
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics)
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            this,
+            Resources.getSystem().displayMetrics
+        )
     }
 
 fun Float.dp(): Float {
     return TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics
     )
+}
+
+val Int.dp
+    get() = this.toFloat().dp
+
+fun getAvatart(resources: Resources, width: Int): Bitmap {
+    val options = BitmapFactory.Options()
+    options.inJustDecodeBounds = true
+    BitmapFactory.decodeResource(resources, R.drawable.avatar, options)
+    options.inJustDecodeBounds = false
+    options.inDensity = options.outWidth
+    options.inTargetDensity = width
+    return BitmapFactory.decodeResource(resources, R.drawable.avatar, options)
 }
